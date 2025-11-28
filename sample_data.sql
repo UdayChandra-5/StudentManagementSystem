34920b5db414435e4eff338e802299fa96aa23c3
UdayChandra-5 <udaychandraofficial@gmail.com>
2025-11-28 18:52:18 +0530

    Add files via upload

---
+++ sample_data.sql
@@ -0,0 +1,15 @@
+INSERT INTO Students (name, age, gender, department) VALUES
+('Bob Mahesh', 20, 'Female', 'Computer Science'),
+('Arjun A', 22, 'Male', 'Electrical Engineering'),
+('Bos Prabha', 21, 'Male', 'Mechanical Engineering');
+
+INSERT INTO Courses (course_name, credits) VALUES
+('Database Systems', 4),
+('Computer Networks', 3),
+('Operating Systems', 4);
+
+INSERT INTO Grades (student_id, course_id, grade) VALUES
+(1, 1, 'A'),
+(1, 2, 'B'),
+(2, 1, 'B'),
+(3, 3, 'A');
